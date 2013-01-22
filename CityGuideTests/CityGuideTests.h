cda340fb61ded25d7b6f50a136bb030fa93a20ea
Misogi <ryudo@Ryudo-no-Mac-mini.local>
2013-01-22 15:46:50 +0900

    Initial Commit

---
+++ CityGuideTests/CityGuideTests.h
@@ -0,0 +1,13 @@
+//
+//  CityGuideTests.h
+//  CityGuideTests
+//
+//  Created by Misogi on 2013/01/22.
+//  Copyright (c) 2013年 Misogi. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CityGuideTests : SenTestCase
+
+@end
---
+++ CityGuideTests/CityGuideTests.m
@@ -0,0 +1,32 @@
+//
+//  CityGuideTests.m
+//  CityGuideTests
+//
+//  Created by Misogi on 2013/01/22.
+//  Copyright (c) 2013年 Misogi. All rights reserved.
+//
+
+#import "CityGuideTests.h"
+
+@implementation CityGuideTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CityGuideTests");
+}
+
+@end
