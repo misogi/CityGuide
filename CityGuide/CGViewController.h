2a9ae7ed315af8196b70a1e5800fc3cbec07eec5
misogi <dev@misogi.info>
2013-01-22 16:39:25 +0900

    add table view

--- CityGuide/CGViewController.h
+++ CityGuide/CGViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CGViewController : UIViewController
+@interface CGViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
