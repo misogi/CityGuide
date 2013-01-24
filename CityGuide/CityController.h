8cec5346eb35a064968bdde7558640e8be6d4f88
misogi <dev@misogi.info>
2013-01-24 14:33:55 +0900

    add New View

---
+++ CityGuide/CityController.h
@@ -0,0 +1,13 @@
+//
+//  CityController.h
+//  CityGuide
+//
+//  Created by Misogi on 2013/01/24.
+//  Copyright (c) 2013年 Misogi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CityController : UIViewController
+
+@end
---
+++ CityGuide/CityController.m
@@ -0,0 +1,38 @@
+//
+//  CityController.m
+//  CityGuide
+//
+//  Created by Misogi on 2013/01/24.
+//  Copyright (c) 2013年 Misogi. All rights reserved.
+//
+
+#import "CityController.h"
+
+@interface CityController ()
+
+@end
+
+@implementation CityController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
