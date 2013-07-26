ad5ba50413f4d1a17cbed273d5c518ee4ecfa26c
misogi <dev@misogi.info>
2013-07-26 16:28:41 +0900

    add a new view for detail of each item on tableView

---
+++ Tabified/Tabified/SimpleView.h
@@ -0,0 +1,13 @@
+//
+//  SimpleView.h
+//  Tabified
+//
+//  Created by Misogi on 2013/07/26.
+//  Copyright (c) 2013年 Misogi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleView : UIViewController
+
+@end
