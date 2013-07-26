99a7c903a90f61d452de3e844fe81670181c6bd9
misogi <dev@misogi.info>
2013-07-26 15:34:01 +0900

    add third view

---
+++ Tabified/Tabified/TBThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  TBThirdViewController.h
+//  Tabified
+//
+//  Created by Misogi on 2013/07/26.
+//  Copyright (c) 2013年 Misogi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBThirdViewController : UIViewController
+
+@end
---
+++ Tabified/Tabified/TBThirdViewController.m
@@ -0,0 +1,38 @@
+//
+//  TBThirdViewController.m
+//  Tabified
+//
+//  Created by Misogi on 2013/07/26.
+//  Copyright (c) 2013年 Misogi. All rights reserved.
+//
+
+#import "TBThirdViewController.h"
+
+@interface TBThirdViewController ()
+
+@end
+
+@implementation TBThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
